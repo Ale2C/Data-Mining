e0a8c2fc7bae1008b7c9357b49079803f03475ea
Ale2C <alejandro.castro17@tectijuana.edu.mx>
2021-12-12 22:08:08 -0800

    Add visualization part for the Exam

--- Evaluacion/Exam-Unit4.r
+++ Evaluacion/Exam-Unit4.r
@@ -1,12 +1,12 @@
 #Chose the location we want woork
-
 getwd()
 setwd("C:/Users/vanem/OneDrive/Documentos/9 SEMESTRE/Mineria/Repo mineria/DataMining/MachineLearning/LogisticRegression")
 getwd()
 
 #Importing the dataset
 dataset <- read.csv(file.choose())
-dataset = dataset[1:4]
+dat1 = dataset[1:4]
+dat2 = dataset[3:4]
 
 # Using the elbow method to find the optimal number of clusters
 TEM <- function(dataset){
@@ -21,7 +21,8 @@ TEM <- function(dataset){
              ylab = 'WCSS')
 }
 
-TEM(dataset)
+TEM(dat1)
+TEM(dat2)
 
 #Fitting K-Means to the dataset
 set.seed(29)
@@ -30,3 +31,29 @@ y_kmeans = kmeans$cluster
 
 
 #Visualizing cluster
+library(cluster)
+#Sepal lenght and Petal width
+clusplot(dat1,
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
+
+#Petal lenght and Petal width
+clusplot(dat2,
+         y_kmeans,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
